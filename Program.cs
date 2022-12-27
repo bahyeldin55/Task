d801054c6c7b98a19caa6cd253f96a4b6c337b54
bahyeldin55 <121494205+bahyeldin55@users.noreply.github.com>
2022-12-27 13:55:41 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter start number:");
+            int startNun = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Enter end number:");
+
+
+            int endNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("These are the numbers between {0} and {1}", startNun, endNum);
+            for (int i = startNun; i <= endNum; i++)
+            {
+                int counter = 0;
+                for (int j = 2; j <= i / 2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        counter++;
+
+
+
+
+                        break;
+                    }
+
+                }
+                if (counter == 0 && i != 1)
+                {
+                    Console.WriteLine(" {0} ", i);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
